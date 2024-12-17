3392652b35bbb027c3ccdeaf4f9c013ff3eaacec
jonhealy1 <jonathan.d.healy@gmail.com>
2024-12-17 20:09:43 +0800

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Oxanium", ...defaultTheme.fontFamily.sans],
+        sans: ["Comfortaa", ...defaultTheme.fontFamily.sans],
       },
     },
   },

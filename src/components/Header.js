96a8250487d5f939e7302976133ad80263b5d12a
jonhealy1 <jonathan.d.healy@gmail.com>
2024-12-17 19:59:36 +0800

    update header font

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/healy-hyper-banner-new.svg";
+import logo from "../assets/healy-hyper-banner.svg";
 import starsVideo from "../assets/stars.mp4"; // Import the video file
 
 const Header = () => {
